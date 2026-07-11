097ae967fef2f72fd2b204dd5555b638ddb8f3d4
vjnagolf-afk <vjnagolf@gmail.com>
2026-07-11 17:02:47 +0700

    Update main.py

--- teaching_assistant/main.py
+++ teaching_assistant/main.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import sys
+import os
+
+# Thêm thư mục gốc của dự án vào đường dẫn tìm kiếm của Python
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 def render_teaching_assistant_section():
     st.title("🌱 Hỗ trợ Giảng dạy")
@@ -16,5 +21,27 @@ def render_teaching_assistant_section():
         
     with tabs[1]:
         st.info("Module Trò chơi đang được phát triển...")
-    
-    # ... Các tab khác tương tự
+        
+    with tabs[2]:
+        st.info("Module Chấm bài đang được phát triển...")
+        
+    with tabs[3]:
+        st.info("Module Học liệu đang được phát triển...")
+        
+    with tabs[4]:
+        st.info("Module Mô phỏng đang được phát triển...")
+        
+    with tabs[5]:
+        st.info("Module Phân tích đang được phát triển...")
+        
+    with tabs[6]:
+        st.info("Module Ngân hàng đề đang được phát triển...")
+        
+    with tabs[7]:
+        st.info("Module Sinh Video đang được phát triển...")
+        
+    with tabs[8]:
+        st.info("Module Tương tác đang được phát triển...")
+        
+    with tabs[9]:
+        st.info("Module Cá nhân hóa đang được phát triển...")
